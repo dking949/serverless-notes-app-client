9576fd9a5a0b2376f6e5934c1ab6032c748ec1b6
Darren King <dking949@gmail.com>
2020-06-01 14:38:52 +1000

    Setting up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "dking-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://on13yzmjwd.execute-api.ap-southeast-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_t8LlU5wG5",
+    APP_CLIENT_ID: "3shdjtch8f9rmu2mvipumc049q",
+    IDENTITY_POOL_ID: "ap-southeast-2:362321ed-7241-4713-99ee-b017ca4e9dc1"
+  }
+};
